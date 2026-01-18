e18c734291e1498b57a1ac5bbf56efc5fb373ca9
NGMG ☢️ SL_MGx <78734430+SL-MGx03@users.noreply.github.com>
2026-01-18 11:17:39 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import win_mark
+import data_frame
+
+# For new macth prediction
+win_mark.predict_future()
+
+# Generate a Classification Report
+win_mark.reportgen()
+
+# Full Data Frame veiw
+data_frame.show_dataframe()
